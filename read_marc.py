14fbc6130436ff094928076ee0fb4fa090e707e9
jmtaysom <james.taysom@gmail.com>
2016-08-15 11:40:28 -0400

    initial upload

---
+++ read_marc.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from pymarc import JSONReader
+
+
+
+def get_record(r):
+    rjson =r.json()
+    marc = rjson['marc']
+    marc['fields'] = [{k:v} for k,v in zip(marc['fields'][0::2],
+                                           marc['fields'][1::2])]
+    for field in marc['fields']:
+        k = list(field.keys())[0]
+        try:
+            field[k]['subfields'] = [{k:v} for k,v in zip(field[k]['subfields'][0::2], 
+                                                          field[k]['subfields'][1::2])]
+        except TypeError:
+            pass
+    
+    jmarc = JSONReader(json.dumps(marc))
+    for record in jmarc:
+        return record
+
+
+if __name__ == '__main__':
+    r = requests.get('http://ilsnext.sandbox.kohalibrary.com/api/work/35736')
+    record = get_record(r)
